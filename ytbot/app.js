ac08b1421e7463bed18e0cc8a33b286fec681e0c
Mamo YZ <yzmamo@gmail.com>
2017-11-30 11:06:59 +0330

    Youtube bot

---
+++ ytbot/app.js
@@ -0,0 +1,50 @@
+var telegram = require('telegram-bot-api');
+
+var readline = require('readline');
+var path = require('path');
+var fs = require('fs');
+var ytdl = require('ytdl-core');
+
+var api = new telegram({
+    token: '505485589:AAFPi4ADXC69WlG2s5HH465aCP96vLyiDRA',
+    updates: {
+        enabled: true
+    }
+});
+
+api.on('message', function(message) {
+    // Received text message
+    var url = message.text;
+    if (ytdl.validateURL(url)) {
+    	api.sendMessage({
+            chat_id: message.chat.id,
+            parse_mode: 'Markdown',
+            text: 'Downloading. Please Wait...'
+
+        });
+        ytdl.getInfo(url, {}, function(err, info) {
+            var filename = info.title + '.mp4';
+            if (filename) {
+                var output = path.resolve(__dirname, filename);
+                var video = ytdl(url);
+                video.pipe(fs.createWriteStream(output));
+                video.on('end', () => {
+                    console.log('DONE!');
+                    api.sendVideo({
+                        chat_id: message.chat.id,
+                        video: filename
+                    });
+                });
+            }
+        });
+    } else {
+        api.sendMessage({
+            chat_id: message.chat.id,
+            parse_mode: 'Markdown',
+            text: 'Bad Format. Please send a Youtube link'
+
+        });
+
+    }
+
+});
