c51852d9e61b54211b2161afbc113657da4e720a
Mamo YZ <yzmamo@gmail.com>
2018-09-08 21:06:36 +0300

    yt playlist downloader

---
+++ yt-playlist-downloader/app.js
@@ -0,0 +1,35 @@
+var path = require('path');
+var fs = require('fs');
+var ytdl = require('youtube-dl');
+var dlurl = 'https://www.youtube.com/playlist?list=' + 'PLQQ6twmUg6FZf2EK4cVCMKtR_W58xt97L';
+function playlist(url) {
+    'use strict';
+    var video = ytdl(url);
+    video.on('error', function error(err) {
+        console.log('error 2:', err);
+    });
+    var size = 0;
+    var filesize = 0;
+    video.on('info', function(info) {
+    	console.log(info.fulltitle);
+        filesize = info.size;
+        size = info.fulltitle.replace('/','').replace('(','').replace(')','').replace('*','').replace('.','').replace(':','').replace('<','').replace('>','').replace('|','');
+        var output = path.join(__dirname + '/', size + '.mp4');
+        video.pipe(fs.createWriteStream(output));
+    });
+    var pos = 0;
+    video.on('data', function data(chunk) {
+        pos += chunk.length;
+        // `size` should not be 0 here.
+        if (size) {
+            var percent = (pos / filesize * 100).toFixed(2);
+            process.stdout.cursorTo(0);
+            process.stdout.clearLine(1);
+            process.stdout.write(percent + '%');
+        }
+    });
+    video.on('next', playlist);
+}
+
+playlist(dlurl);
+
---
+++ yt-playlist-downloader/convert.sh
@@ -0,0 +1,1 @@
+for f in *.mp4; do ffmpeg -i "$f" -f mp3 -ab 192000 -vn "${f%.mp4}.mp3"; done
